bf8cf35513827129be2188c33ff665861e9afcf3
benroberson <benroberson2@gmail.com>
2023-12-16 09:13:35 -0500

    add fonts

---
+++ js/count.js
@@ -0,0 +1,29 @@
+const queryString = window.location.search;
+var urlString;
+if (queryString === "?nocount")
+	urlString = "https://api.countapi.xyz/get/utkbrobers3/views";
+else
+	urlString = "https://api.countapi.xyz/hit/utkbrobers3/views";
+	
+var xhr = new XMLHttpRequest();
+xhr.open("GET", urlString);
+xhr.responseType = "json";
+xhr.onload = function () {
+	console.log("view count: " + this.response.value);
+}
+xhr.send();
+
+function reset() {
+	var xhr1 = new XMLHttpRequest();
+	xhr1.open("GET", "https://api.countapi.xyz/set/utkbrobers3/views?value=0");
+	xhr1.responseType = "json";
+	xhr1.onload = function() {
+		console.log("reset view count=" + this.response.value );
+		var xhr2 = new XMLHttpRequest();
+		xhr2.open("GET", "https://api.countapi.xyz/update/utkbrobers3/viewsall?amount=" + this.response.old_value );
+		xhr2.responseType = "json";
+		xhr2.onload = function() { console.log("total view count=" + this.response.value ); }
+		xhr2.send();
+	}
+	xhr1.send();
+}
